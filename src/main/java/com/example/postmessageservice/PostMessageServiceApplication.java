6c1dd5651890098770fa098a171b071bddf27e4e
Osedumme Steven <stephen.osedumme@iths.se>
2023-06-06 12:43:47 +0200

    Initial Commit

---
+++ src/main/java/com/example/postmessageservice/PostMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.postmessageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMessageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostMessageServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/postmessageservice/PostMessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postmessageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMessageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
