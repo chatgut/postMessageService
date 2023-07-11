af839471761dab5e891e9b2b2a12252949f3d013
Osedumme Steven <stephen.osedumme@iths.se>
2023-07-11 14:16:00 +0200

    changes

---
+++ postmessages/src/main/java/com/ithswebservice/postmessages/PostmessagesApplication.java
@@ -0,0 +1,13 @@
+package com.ithswebservice.postmessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmessagesApplication.class, args);
+	}
+
+}
---
+++ postmessages/src/test/java/com/ithswebservice/postmessages/PostmessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ithswebservice.postmessages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
